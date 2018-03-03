cb1e105944d3c32ff5626d6cdcceb36c0e402ec3
Josh Schneider <josh@joshschneider.xyz>
2018-03-03 10:20:26 -0500

    Created database singleton class

--- app/teiku/database.php
+++ app/teiku/database.php
@@ -1,8 +1,38 @@
 <?php
+
 namespace Teiku;
+
 use PDO;
 
 class Database
 {
+
+    private static $instance = null;
+    private $connection;
+
+    private function __construct()
+    {
+        $creds = json_decode(file_get_contents(APP . '/db/credentials'));
+        if ($creds->protocol === 'sqlite') {
+            $this->connection = new PDO($creds->dsn);
+            $this->connection->exec('PRAGMA foreign_keys = 1');
+        } else {
+            $this->connection = new PDO($creds->dsn, $creds->user, $creds->password);
+        }
+    }
+
+    private function __clone()
+    {
+    }
+
+    public static function connect(): Database
+    {
+        return self::$instance = self::$instance ?? new Database();
+    }
+
+    public function open(): PDO
+    {
+        return $this->connection;
+    }
 
 }
